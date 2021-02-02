11e1a9b908e55ea3282d3978fd29dda5e92c7795
eunho3854 <eunho3854@naver.com>
2021-02-02 12:23:18 +0900

    useRef, createRef

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 // useRef (디자인)
+// dom을 변경할 때 사용
 import { useEffect, useRef } from 'react';
 
 function App() {
   const myRef = useRef(null);
-  const boxRef = useRef(null);
-
-  useEffect(() => {
-    myRef.current.focus();
-    myRef.current.style.backgroundColor = 'black';
-  }, []);
 
   return (
     <div>
-      <input type="text" value={'안녕'} ref={myRef}></input>
-      <div ref={boxRef}>박스</div>
       <button
         onClick={() => {
-          boxRef.current.style.backgroundColor = 'red';
+          myRef.current.style.backgroundColor = 'red';
         }}
       >
-        박스 색깔 변경
+        색 변경
       </button>
+      <div ref={myRef}>박스</div>
     </div>
   );
 }
